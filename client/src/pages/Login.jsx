298c0cbbe704634c897335a6f3f7a3741e2abae1
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-08-07 16:30:32 +0200

    login pageé

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 const Login = () => {
   return (
-    <div>
-      <h1>login</h1>
+    <div className="login">
+      <div className="login__intro">
+        <Link to="/">
+          <AiOutlineArrowLeft className="login__intro-arrow" />
+        </Link>
+        <h1 className="login__intro-title">Heureux de vous revoir !</h1>
+      </div>
+      <form>
+        <div className="login__infos">
+          <input type="email" name="email" placeholder="Email..." />
+          <input
+            type="password"
+            name="password"
+            placeholder="Mot de passe..."
+          />
+        </div>
+        <input type="submit" value="Se connecter" className="login__submit" />
+      </form>
+      <div className="login__signup">
+        <p> Pas encore de compte ?</p>
+        <Link to="/signup"> Clique ici ! </Link>
+      </div>
     </div>
   )
 }
--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -5,16 +5,12 @@ const Welcome = () => {
   return (
     <div className="welcome">
       <h1 className="welcome__title">Bienvenue chez Groupomania !</h1>
-      <button className="welcome__login-btn">
-        <Link to="/login" className="welcome__login-link">
-          Se connecter
-        </Link>
-      </button>
-      <button className="welcome__signup-btn">
-        <Link to="/signup" className="welcome__signup-link">
-          Créer un compte
-        </Link>
-      </button>
+      <Link to="/login" className="welcome__login-link">
+        Se connecter
+      </Link>
+      <Link to="/signup" className="welcome__signup-link">
+        Créer un compte
+      </Link>
     </div>
   )
 }
