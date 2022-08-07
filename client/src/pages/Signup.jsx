da5880830c85f87e33f6102404e9ba06d2621101
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-08-07 16:55:15 +0200

    signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,55 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 
 const Signup = () => {
-  return <div>Signup</div>
+  return (
+    <div className="signup">
+      <div className="signup__intro">
+        <Link to="/">
+          <AiOutlineArrowLeft className="signup__intro-arrow" />
+        </Link>
+        <h1 className="signup__intro-title">Rejoignez-nous !</h1>
+      </div>
+      <form>
+        <input
+          type="text"
+          name="userSurname"
+          placeholder="Prénom..."
+          className="signup__info"
+        />
+        <input
+          type="text"
+          name="userName"
+          placeholder="Nom..."
+          className="signup__info"
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email..."
+          className="signup__info"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Mot de passe..."
+          className="signup__info"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Confirmez..."
+          className="signup__info"
+        />
+        <input
+          type="submit"
+          value="Créer un compte"
+          className="signup__submit"
+        />
+      </form>
+    </div>
+  )
 }
 
 export default Signup
