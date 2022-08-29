14fbac9c8c11b5e40f9db41266a2fd9f38fa8c3e
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-08-29 15:31:31 +0200

    edit post model no need comment field now'

--- server/models/Post.js
+++ server/models/Post.js
@@ -10,15 +10,7 @@ const postSchema = mongoose.Schema({
     content: { type: String },
     imgUrl : { type: String, default: '' },
     likes: { type: Number, default: 0 },
-    userLikes: [],
-    comments: [{
-        profilePhotoUrl: { type: String },
-        userSurname: { type: String },
-        userName: { type: String },
-        Date: { type: String },
-        modificationDate: { type: String, default: '' },
-        content: { type: String },
-    },], 
+    userLikes: [], 
 })
 
 module.exports = mongoose.model('post', postSchema);
