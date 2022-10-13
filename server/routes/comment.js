8722dd6a58ff029cfbb3d4fc4d88fbf76866380a
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-10-13 13:39:00 +0200

    edit

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const commentCtrl = require('../controllers/comment');
+
 // COMMENT SYSTEM (WITH POSTID)
 router.post('/:id', commentCtrl.postComment);
+
 // all comments on a post 
 router.get('/:id', commentCtrl.getComments);
 router.put('/:id', commentCtrl.editComment);
